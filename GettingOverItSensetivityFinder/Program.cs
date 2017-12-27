088ba6935e4d900a90a912a01407ae1448df411b
TheRedCoder <theredcoder2000@gmail.com>
2017-12-28 00:45:09 +0200

    fixed wrong hex format

--- GettingOverItSensetivityFinder/Program.cs
+++ GettingOverItSensetivityFinder/Program.cs
@@ -16,7 +16,6 @@ namespace GettingOverItSensetivityFinder {
 
         static string hexValue(long l) {
             byte[] bytes = BitConverter.GetBytes(l);
-            Array.Reverse(bytes);
             StringBuilder hex = new StringBuilder(bytes.Length * 2);
             foreach (byte b in bytes)
                 hex.AppendFormat("{0:x2}", b);
